5b1bf0bbe1441f317731709d070f2d65cf352f7d
haziqruzi <71532216+zh6ziq@users.noreply.github.com>
2023-06-01 14:12:25 +0800

    added customKey

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const { CUSTOM_KEY } = process.env;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  try {
+    const customKey = CUSTOM_KEY;
+
+    res.status(200).json({
+      status: 'success',
+      data: customKey,
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
 });
 
 module.exports = router;
